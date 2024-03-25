1e902717b77d5302aa67de19e67fefb1a9bc630c
Victoria Stavish <Vstavish@terpmail.umd.edu>
2024-03-25 18:22:08 +0000

    update

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask
 from flask import render_template
+from peewee import *
 app = Flask(__name__)  # Note the double underscores on each side!
+
+db = SqliteDatabase('foreclosures.db')
+
+class Notice(Model):
+    id = IntegerField(unique=True)
+    zip = CharField()
+    month = DateField()
+    notices = IntegerField()
+
+    class Meta:
+        database = db
 
 @app.route("/")
 def index():
+    notice_count = Notice.select().count()
+    notices_20906 = Notice.select().where(Notice.zip=='20906')
     template = 'index.html'
-    return render_template(template)
+    return render_template(template, count = notice_count, notices = notices_20906)
 
 if __name__ == '__main__':
-    # Fire up the Flask test server
     app.run(debug=True, use_reloader=True)
