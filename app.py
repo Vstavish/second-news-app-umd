f8690c0d5df99699c39a449cd6b556164b92149f
Victoria Stavish <Vstavish@terpmail.umd.edu>
2024-03-25 18:35:19 +0000

    update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template
 from peewee import *
-app = Flask(__name__)  # Note the double underscores on each side!
+app = Flask(__name__)
 
 db = SqliteDatabase('foreclosures.db')
 
@@ -12,24 +12,19 @@ class Notice(Model):
     notices = IntegerField()
 
     class Meta:
+        table_name = "notices"
         database = db
 
 @app.route("/")
 def index():
-    notice_count = Notice.select().count()
-    all_zips = (Notice.select(Notice.zip).distinct())
+    print("Total number of notices is", Notice.select().count())
+    notice = Notice.select().where(Notice.id==3963).get()
+    print(f"Zip code {notice.zip} had {notice.notices} in {notice.month}")
+    notices_20906 = Notice.select().where(Notice.zip=='20906')
+    for notice in notices_20906:
+    print(notice.notices)
     template = 'index.html'
-    return render_template(template, count = notice_count, all_zips = all_zips)
-
-@app.route('/zipcode/<slug>')
-def detail(slug):
-    zipcode = slug
-    notices = Notice.select().where(Notice.zip==slug)
-    total_notices = Notice.select(fn.SUM(Notice.notices).alias('sum')).where(Notice.zip==slug).scalar()
-    notice_json = []
-    for notice in notices:
-        notice_json.append({'x': str(notice.month.year) + ' ' + str(notice.month.month), 'y': notice.zip, 'heat': notice.notices})
-    return render_template("detail.html", zipcode=zipcode, notices=notices, notices_count=len(notices), notice_json = notice_json, total_notices = total_notices)
+    return render_template(template)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
