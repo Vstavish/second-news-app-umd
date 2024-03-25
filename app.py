a638ba71409723c9b9c3bfb46281b6c3a79db8ec
Victoria Stavish <Vstavish@terpmail.umd.edu>
2024-03-25 18:50:19 +0000

    update

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def detail(slug):
     for notice in notices:
         notice_json.append({'x': str(notice.month.year) + ' ' + str(notice.month.month), 'y': notice.zip, 'heat': notice.notices})
     owner_occupied = c.acs5.state_zipcode(('NAME', 'B25003_002E'), '24', zipcode)
-    return render_template("detail.html", zipcode=zipcode, notices=notices, notices_count=len(notices), notice_json = notice_json, total_notices = total_notices, owner_occupied = owner_occupied[0]['B25003_002E'])
+    return render_template("detail.html", zipcode=zipcode, notices=notices, notices_count=len(notices), notice_json = notice_json, total_notices = total_notices, owner_occupied = int(owner_occupied[0]['B25003_002E']))
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
